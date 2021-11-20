70b4e94b7fb5403c75ce56e30964b831dee8fa3e
mattgefen <matt.gefen@gmail.com>
2021-11-20 13:53:01 -0700

    stubbing finches index with faked data for now

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, species, description):
+    self.name = name
+    self.species = species
+    self.description = description
+
+finches = [
+  Finch('Dorkus', 'House Finch', 'A wee little scamp')
+] 
+
 def home(request):
   return HttpResponse('finch finch finch')
 def about(request):
-  return HttpResponse('<h1>About the Finch Collector<h1>')
+  return render(request, 'about.html')
+def finches_index(request):
+  return render(request, 'finches/index.html', {finches: finches})
